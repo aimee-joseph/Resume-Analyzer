c8b67ef801e2fadb1d7d9b33b0cf0b65e8db5160
= <=>
2026-01-17 17:25:26 +0530

    Added an additional function to extract skills

--- resume_analyzer/utils/skill_extractor.py
+++ resume_analyzer/utils/skill_extractor.py
@@ -9,13 +9,10 @@ def load_skills(file_path: str) -> Set[str]:
         skills = {line.strip().lower() for line in file if line.strip()}
 
     return skills
-#     with open(file_path, "r", encoding = "utf-8") as file:
-#         skills = {line.strip().lower() for line in file if line.strip()}
-#     return skills
 
-# def extract_skills(text: str, skills_db: Set[str]) -> Set[str]:
-#     extracted = set()
-#     for skill in skills_db:
-#         if skill in text:
-#             extracted.add(skill)
-#     return extracted
+def extract_skills(text: str, skills_db: Set[str]) -> Set[str]:
+    extracted = set()
+    for skill in skills_db:
+        if skill in text:
+            extracted.add(skill)
+    return extracted
